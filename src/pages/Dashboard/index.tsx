9607498d0a2e0fa18d0e7da779a6468b0351a7dd
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-13 22:07:28 -0300

    feat: dashboard stylized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,57 @@
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 export const Dashboard: React.FC = () => {
   return (
-    <div>
+    <>
+      <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-    </div>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/72233319?v=4"
+            alt="Gui Prais"
+          />
+          <div>
+            <strong>guiprais/github-explorer</strong>
+            <p>Github Explorer made during GoStack bootcamp</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/72233319?v=4"
+            alt="Gui Prais"
+          />
+          <div>
+            <strong>guiprais/github-explorer</strong>
+            <p>Github Explorer made during GoStack bootcamp</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/72233319?v=4"
+            alt="Gui Prais"
+          />
+          <div>
+            <strong>guiprais/github-explorer</strong>
+            <p>Github Explorer made during GoStack bootcamp</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
